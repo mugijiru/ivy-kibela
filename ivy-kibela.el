f4095d812bdfc040d2be488ed7601d28df5c874b
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-08-07 16:42:50 +0900

    Use Eask
    Eask を使いビルド周りを整えることにした

--- ivy-kibela.el
+++ ivy-kibela.el
@@ -1,12 +1,17 @@
-;;; ivy-kibela.el --- ivy interface for kibe.la      -*- lexical-binding: t; -*-
+;;; ivy-kibela.el --- Ivy interface to Kibela  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022  Mugijiru
+;; Copyright (C) 2022  mugijiru
 
-;; Author: Mugijiru <mugijiru@manjaro>
-;; Package-Requires: ((emacs "28.1") (ivy "0.13.0") (graphql "0.1.1") (request 0.3.3))
+;; Author: mugijiru <106833+mugijiru@users.noreply.github.com>
+;; Maintainer: mugijiru <106833+mugijiru@users.noreply.github.com>
+;; URL: https://github.com/mugijiru/ivy-kibela
 ;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1") (ivy "0.13.0") (graphql "0.1.1") (request "0.3.3"))
+;; Keywords: matching ivy kibela
 
-;; This program is free software; you can redistribute it and/or modify
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
@@ -17,11 +22,12 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; Find kibela note interface for ivy.
+;;
+;; Ivy interface to Kibela
+;;
 
 ;;; Code:
 
